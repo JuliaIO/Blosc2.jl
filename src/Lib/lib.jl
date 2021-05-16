058b265a443f0526e6678cd8a371f410ddbe693d
Alexandr Romanenko <waralex@gmail.com>
2021-05-16 14:39:52 +0300

    change lib name

--- src/Lib/lib.jl
+++ src/Lib/lib.jl
@@ -5,7 +5,7 @@ Low-level blosc api - wrappers under corresponding c functions
 """
 module Lib
     using Blosc2_jll
-    const lib = blosc
+    const lib = libblosc2
     include("defines.jl")
     include("blosc1/blosc1.jl")
 end
